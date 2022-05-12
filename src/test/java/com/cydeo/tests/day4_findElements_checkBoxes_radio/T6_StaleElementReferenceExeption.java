2d590daa86a6404f6fd8c1bf4eb5dfac41275c74
Jasmine <bayramali5219@gmail.com>
2022-05-12 07:08:02 +0300

    Day4 Task 6 Stale Element ReferenceExcepytion

--- src/test/java/com/cydeo/tests/day4_findElements_checkBoxes_radio/T6_StaleElementReferenceExeption.java
+++ src/test/java/com/cydeo/tests/day4_findElements_checkBoxes_radio/T6_StaleElementReferenceExeption.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day4_findElements_checkBoxes_radio;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,9 +17,17 @@ public class T6_StaleElementReferenceExeption {
         //2- Go to: https://practice.cydeo.com/abtest
         driver.get("https://practice.cydeo.com/abtest" );
         //3- Locate “CYDEO” link, verify it is displayed.
+        WebElement cydeoLink = driver.findElement(By.xpath("//a[text()='CYDEO']"));
+
+        System.out.println("cydeoLink.isDisplayed() = " + cydeoLink.isDisplayed());
 
         //4- Refresh the page.
+        driver.navigate().refresh();
+        //We are refreshing the web element reference by re-assigning (re-locating) the web element
+        cydeoLink = driver.findElement(By.xpath("//a[text()='CYDEO']"));
+
         //5- Verify it is displayed, again.
+        System.out.println("cydeoLink.isDisplayed() = " + cydeoLink.isDisplayed());
 
     }
 }
