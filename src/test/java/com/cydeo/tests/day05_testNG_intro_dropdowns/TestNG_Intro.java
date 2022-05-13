5791e2cbdf9f85a08fcb2886360aed19e022ccff
Jasmine <bayramali5219@gmail.com>
2022-05-13 12:42:42 +0300

    Day5 TestNG Intro

---
+++ src/test/java/com/cydeo/tests/day05_testNG_intro_dropdowns/TestNG_Intro.java
@@ -0,0 +1,10 @@
+package com.cydeo.tests.day05_testNG_intro_dropdowns;
+
+import org.testng.annotations.Test;
+
+public class TestNG_Intro {
+    @Test
+    public void test1(){
+        System.out.println();
+    }
+}
