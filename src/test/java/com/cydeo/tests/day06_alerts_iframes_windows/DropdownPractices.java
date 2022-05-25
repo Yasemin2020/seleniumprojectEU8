d8f532b5199e3f910b01e05c2ef202a95cf5f1a2
Jasmine <bayramali5219@gmail.com>
2022-05-25 13:35:31 +0300

    Day6 dropdown task5-6

--- src/test/java/com/cydeo/tests/day06_alerts_iframes_windows/DropdownPractices.java
+++ src/test/java/com/cydeo/tests/day06_alerts_iframes_windows/DropdownPractices.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -73,12 +74,19 @@ public class DropdownPractices {
          String actualMonth= monthDropdown.getFirstSelectedOption().getText();
          String actualDay= dayDropdown.getFirstSelectedOption().getText();
 
+         //create assertions
+         Assert.assertTrue(actualYear.equals(actualYear));
+         Assert.assertEquals(actualMonth, expectedMonth);
+         Assert.assertEquals(actualDay,expectedDay);
+
 
 
 
      }
-
-
+     @AfterMethod
+     public void teardownMethod(){
+         driver.close();
+     }
 
 
 
