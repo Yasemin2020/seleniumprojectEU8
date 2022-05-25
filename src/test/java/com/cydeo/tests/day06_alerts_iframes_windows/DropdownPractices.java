c35216558a6e9d1d5fdf2101f3f4933f12baefa4
Jasmine <bayramali5219@gmail.com>
2022-05-25 12:14:40 +0300

    Day6 dropdown task5-6

--- src/test/java/com/cydeo/tests/day06_alerts_iframes_windows/DropdownPractices.java
+++ src/test/java/com/cydeo/tests/day06_alerts_iframes_windows/DropdownPractices.java
@@ -1,22 +1,69 @@
 package com.cydeo.tests.day06_alerts_iframes_windows;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class DropdownPractices {
-    public static void main(String[] args) {
-        //TC #5: Selecting state from State dropdown and verifying result
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+
         //1. Open Chrome browser
         //2. Go to http://practice.cydeo.com/dropdown
-        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
-
-        //3. Select Illinois
-        //4. Select Virginia
-        //5. Select California
-        //6. Verify final selected option is California.
-        //Use all Select options. (visible text, value, index)
-
-
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://practice.cydeo.com/dropdown");
     }
+    @Test
+    public void dropdown_task5() throws InterruptedException {
+         //we located the dropdown and it is ready to use
+        Select stateDropdown = new Select(driver.findElement(By.xpath("//select[@id='state']")));
+        //Use all Select options. (visible text, value, index)
+        //3. Select Illinois
+        stateDropdown.selectByVisibleText("Illinois");
+
+        //4. Select Virginia
+        stateDropdown.selectByValue("VA");
+
+        //5. Select California
+        stateDropdown.selectByIndex(5);
+
+        //6. Verify final selected option is California.
+        String expectedOptionText = "California";
+        String actualOptionText = stateDropdown.getFirstSelectedOption().getText();
+
+        Assert.assertEquals(actualOptionText, expectedOptionText);
+    }
+     @Test
+    public void dropdown_task6(){
+
+        //1. Open Chrome browser
+         //2. Go to https://practice.cydeo.com/dropdown
+         //3. Select “December 1st, 1923” and verify it is selected.
+         Select yearDropdown = new Select(driver.findElement(By.xpath("")));
+         Select monthDropdown = new Select(driver.findElement(By.xpath("")));
+         Select dayDropdown = new Select(driver.findElement(By.xpath("")));
+
+
+
+         //Select year using  : visible text
+         //Select month using   : value attribute
+         //Select day using : index number
+
+     }
+
+
+
+
+
+
 }
