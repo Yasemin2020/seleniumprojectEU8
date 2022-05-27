ee2f759e0aa0a03af4f10adbc762cb4ecf8c8459
Jasmine <bayramali5219@gmail.com>
2022-05-27 19:48:43 +0300

    Day7 task1

---
+++ src/test/java/com/cydeo/tests/day07_webtables_utilities_javafaker/T1_window_handling_test.java
@@ -0,0 +1,67 @@
+package com.cydeo.tests.day07_webtables_utilities_javafaker;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T1_window_handling_test {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+
+        //1. Open Chrome browser
+        //2. Go to : https://www.amazon.com
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.amazon.com");
+    }
+
+    @Test
+    public void window_handling_test(){
+        //2. Go to : https://www.amazon.com
+        driver.get("https://www.amazon.com");
+
+        //3. Copy paste the lines from below into your class
+        ((JavascriptExecutor) driver).executeScript("window.open('http://google.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('http://etsy.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('http://facebook.com','_blank');");
+
+        //4. Create a logic to switch to the tab where Etsy.com is open
+
+        for (String windowHandle : driver.getWindowHandles()) {
+
+            driver.switchTo().window(windowHandle);
+            System.out.println("Current URL: "+ driver.getCurrentUrl());
+
+            if (driver.getCurrentUrl().contains("etsy")){
+                break;
+            }
+
+        }
+
+        //5. Assert: Title contains “Etsy”
+        String actualTitle=driver.getTitle();
+        String expectedInTitle="Etsy";
+
+        Assert.assertEquals(actualTitle, expectedInTitle);
+
+        //Lines to be pasted:
+        //((JavascriptExecutor) driver).executeScript("window.open('http://google.com','_blank');");
+        //((JavascriptExecutor) driver).executeScript("window.open('http://etsy.com','_blank');");
+        //((JavascriptExecutor) driver).executeScript("window.open('http://facebook.com','_blank');");
+
+        //These lines will simply open new tabs using something called JavascriptExecutor
+
+
+    }
+
+
+}
