d074c9be53230567e0fd86cd262ed00ce59ab83d
Jasmine <bayramali5219@gmail.com>
2022-06-10 10:24:19 +0300

    POM  task 1

---
+++ src/test/java/com/cydeo/Pages/LibraryLoginPage.java
@@ -0,0 +1,21 @@
+package com.cydeo.Pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LibraryLoginPage {
+    //#- initialize the driver instance and object of the class
+    public LibraryLoginPage(){
+
+        /*initElements method will create connection in between the current
+         driver session (instance) and the object of the current class*/
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    //#2- use @FindBy annotation to locate web elements
+    @FindBy(xpath = "//input[@id='inputEmail']")
+    public WebElement inputUserName;
+
+}
